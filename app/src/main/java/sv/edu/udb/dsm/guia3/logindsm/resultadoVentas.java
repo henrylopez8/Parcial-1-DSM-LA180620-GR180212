4d126db596a5a876e0b86abbce8f232a8e99ee7a
Brayan Gamez <bgamez1010@gmail.com>
2022-03-06 16:29:10 -0600

    Se agrego la integracion de resultado ventas

--- app/src/main/java/sv/edu/udb/dsm/guia3/logindsm/resultadoVentas.java
+++ app/src/main/java/sv/edu/udb/dsm/guia3/logindsm/resultadoVentas.java
@@ -2,8 +2,16 @@ package sv.edu.udb.dsm.guia3.logindsm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class resultadoVentas extends AppCompatActivity {
 
@@ -11,22 +19,80 @@ public class resultadoVentas extends AppCompatActivity {
     private TextView MostrarCodigo;
     private TextView MostrarVentas;
     private TextView MostrarMes;
+    private TextView MostrarComision;
+    private ImageView Foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_ventas);
 
-        MostrarNombre= (TextView) findViewById(R.id.txtnombre);
-        MostrarCodigo= (TextView) findViewById(R.id.txtcodigoempleado);
-        MostrarVentas= (TextView) findViewById(R.id.txtventas);
-        MostrarMes= (TextView) findViewById(R.id.txtmesventas);
+        Foto = (ImageView) findViewById(R.id.image);
+        MostrarNombre = (TextView) findViewById(R.id.txtNombre);
+        MostrarCodigo = (TextView) findViewById(R.id.txtCodigo);
+        MostrarVentas = (TextView) findViewById(R.id.txtVentas);
+        MostrarMes    = (TextView) findViewById(R.id.txtMes);
+        MostrarComision = (TextView) findViewById(R.id.txtComision);
 
-        Bundle bundle= getIntent().getExtras();
-        String nombre= bundle.getString("txtnombre");
-        String codigo = bundle.getString("txtCodigo");
-        double ventas = Double.parseDouble(bundle.getString("txtnumeroVentas"));
-        String mes = bundle.getString("txtmesventas");
+        Intent intent = getIntent();
+        String ruta = intent.getStringExtra("imageViewFoto");
+        String name = intent.getStringExtra("txtnombre");
+        String code = intent.getStringExtra("txtcodigo");
+        String sell = intent.getStringExtra("txtventas");
+        String month = intent.getStringExtra("txtmes");
+        Double help = Double.parseDouble(sell);
+        Double Commision = getCommision(help);
+
+        //Mostramos los datos
+        MostrarNombre.setText("Nombre: "+name);
+        MostrarCodigo.setText("Codigo Empleado: "+code);
+        MostrarVentas.setText("N Ventas: "+sell);
+        MostrarMes.setText("Mes de venta: $"+month);
+        MostrarComision.setText(Commision.toString());
+        Foto.setImageURI(Uri.parse(ruta));
+
 
     }
+
+    public void cerrarSesion(View v)
+    {
+        SharedPreferences preferences =
+                getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        preferences.edit().remove("Credenciales").commit();
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public Double getCommision(Double quantity)
+    {
+        Double commision = 0.00;
+        if(quantity < 500)
+        {
+            commision = 00.00;
+        }
+        else if(quantity>=500 && quantity < 1000)
+        {
+            commision = quantity*0.05;
+        }
+        else if(quantity>=1000 && quantity < 2000)
+        {
+            commision = quantity*0.1;
+        }
+        else if(quantity>=2000 && quantity < 3000)
+        {
+            commision = quantity*0.15;
+        }
+        else if(quantity>=3000 && quantity < 4000)
+        {
+            commision = quantity*0.20;
+        }
+        else if(quantity>=4000)
+        {
+            commision = quantity*0.30;
+        }
+
+        return  commision;
+    }
+
 }
--- app/src/main/java/sv/edu/udb/dsm/guia3/logindsm/ventas.java
+++ app/src/main/java/sv/edu/udb/dsm/guia3/logindsm/ventas.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.nio.charset.StandardCharsets;
 
 public class ventas extends AppCompatActivity {
 
@@ -15,6 +20,7 @@ public class ventas extends AppCompatActivity {
     private EditText Codigo;
     private EditText numerodeventas;
     private EditText mesVentas;
+    Uri path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +32,36 @@ public class ventas extends AppCompatActivity {
         numerodeventas= (EditText) findViewById(R.id.txtventas);
         mesVentas= (EditText) findViewById(R.id.txtmesventas);
 
+    }
 
+    public void onCLick(View v)
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione la aplicacion"),10);
     }
 
     public void btncalcularsalarios (View view){
 
         Intent i = new Intent(this, resultadoVentas.class);
-        i.putExtra("txtnombrecompleto",nombreCompleto.getText().toString());
-        i.putExtra("txtCodigo",Codigo.getText().toString());
-        i.putExtra("txtnumeroVentas",numerodeventas.getText().toString());
-        i.putExtra("txtmesventas",mesVentas.getText().toString());
+        i.putExtra("txtnombre",nombreCompleto.getText().toString());
+        i.putExtra("txtcodigo",Codigo.getText().toString());
+        i.putExtra("txtventas",numerodeventas.getText().toString());
+        i.putExtra("txtmes",mesVentas.getText().toString());
+        i.putExtra("imageViewFoto",path.toString());
         startActivity(i);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK)
+        {
+             path = data.getData();
+        }
     }
 
     public void cerrarSesion(View v)
